8f3e2553f783e2d3ef1a4a57f35ee60674b71cc6
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-04-11 14:58:13 +0200

    Noise added when buttons clicked

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,10 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController{
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +19,13 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav"){
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            
+            AudioServicesPlaySystemSound(mySound)
+        }
         
     }
     
